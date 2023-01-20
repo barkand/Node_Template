2c30e077956585b2e0247b00b000a2184421e6cd
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-20 18:59:05 +0330

    Load Dynamic Subsystems

--- src/core/routes/index.ts
+++ src/core/routes/index.ts
@@ -1,27 +1,25 @@
 import express from "express";
+import { existsSync } from "fs";
 
 import AdminRouters from "../../admin/routes";
-import MarketRouters from "../../market/routes";
-import GameRouters from "../../game/routes";
 import LogRouters from "../../log/routes";
 
 const routers = express.Router();
 routers.use(express.json());
 
-routers.get("/", (req: any, res: any) => {
-  res.send(
-    `
-      /admin
-      /market
-      /game
-      /log
-    `
-  );
-});
-
 routers.use("/admin", AdminRouters);
-routers.use("/market", MarketRouters);
-routers.use("/game", GameRouters);
 routers.use("/log", LogRouters);
+
+const marketPath = "../../market";
+if (existsSync(marketPath)) {
+  const MarketRouters = require(`${marketPath}/routes`);
+  routers.use("/market", MarketRouters);
+}
+
+const gamePath = "../../game";
+if (existsSync(gamePath)) {
+  const GameRouters = require(`${gamePath}/routes`);
+  routers.use("/game", GameRouters);
+}
 
 export default routers;
