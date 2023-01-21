747041ad42a75ab20845008456ef1fbeb86c8b20
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-01-22 00:32:42 +0330

    Add Redis env

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,9 @@ declare namespace NodeJS {
     DB_NAME: string;
     DB_USER: string;
     DB_PASSWORD: string;
+    DB_CACHE_HOST: string;
+    DB_CACHE_PORT: string;
+    DB_CACHE_PASSWORD: string;
     SECRET_KEY: string;
     SECRET_KEY_LIFE_TIME: string;
     REFRESH_SECRET_KEY: string;
