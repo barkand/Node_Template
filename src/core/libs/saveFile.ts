c2d73130ee487bfdbca5d788fc6296dd21656223
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-02-02 00:43:27 +0330

    Fix Path of  File Upload

--- src/core/libs/saveFile.ts
+++ src/core/libs/saveFile.ts
@@ -1,15 +1,26 @@
-import fs from "fs";
+import { mkdirSync, existsSync, writeFile } from "fs";
 import { join } from "path";
 
 import logger from "../../log";
+import response from "./responses";
 
 const path = "Core>libs>saveFile>";
 
-const SaveFile = (data: string, folderName: string, fileName: string) => {
-  const full_path = join("public", folderName ?? "", fileName);
-  fs.writeFile(full_path, data, function (err) {
-    if (err) logger.error(`${path}SaveFile: ${err}`);
-  });
+const SaveFile = async (data: string, folderName: string, fileName: string) => {
+  const uploadPath = join("public", folderName ?? "");
+  if (!existsSync(uploadPath)) mkdirSync(uploadPath, { recursive: true });
+
+  const full_path = join(uploadPath, fileName);
+  try {
+    writeFile(full_path, data, function (err) {
+      if (err) logger.error(`${path}SaveFile: ${err}`);
+    });
+
+    return response.success;
+  } catch (e: any) {
+    logger.error(`${path}SaveFile: ${e}`);
+    return response.custom(500, `SaveFile: ${e.message}`);
+  }
 };
 
 export default SaveFile;
