8393f1cb8259e4da7f1265964e789a4ea046943a
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-02-01 22:24:10 +0330

    Save Files in Public Folder

--- src/core/libs/saveFile.ts
+++ src/core/libs/saveFile.ts
@@ -1,11 +1,13 @@
 import fs from "fs";
+import { join } from "path";
 
 import logger from "../../log";
 
 const path = "Core>libs>saveFile>";
 
-const SaveFile = (data: string, file_path: string) => {
-  fs.writeFile(file_path, data, function (err) {
+const SaveFile = (data: string, folderName: string, fileName: string) => {
+  const full_path = join("public", folderName ?? "", fileName);
+  fs.writeFile(full_path, data, function (err) {
     if (err) logger.error(`${path}SaveFile: ${err}`);
   });
 };
