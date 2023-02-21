057ee77828b355fc5f2d1ba75ed77a580d67be6c
Hamid Barkand <30972715+barkand@users.noreply.github.com>
2023-02-21 21:00:18 +0330

    Add metadata Static & Fix Upload Path

--- src/core/middleware/static.ts
+++ src/core/middleware/static.ts
@@ -4,13 +4,15 @@ import { join } from "path";
 const staticMiddleware = async (app: any) => {
   app.use(
     `/${process.env.UPLOAD_FOLDER}/users`,
-    express.static(join(process.env.UPLOAD_FOLDER, "users"))
+    express.static(join("public", process.env.UPLOAD_FOLDER, "users"))
   );
 
   app.use(
     `/${process.env.UPLOAD_FOLDER}/teams`,
-    express.static(join(process.env.UPLOAD_FOLDER, "teams"))
+    express.static(join("public", process.env.UPLOAD_FOLDER, "teams"))
   );
+
+  app.use(`/metadata`, express.static(join("public", "metadata")));
 };
 
 export default staticMiddleware;
